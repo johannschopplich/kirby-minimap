fa61462b1095677a4d1d85dc13d39f87c7aa27a4
Johann Schopplich <mail@johannschopplich.com>
2025-12-31 18:20:10 +0100

    refactor: outsource model and field resolvers

--- src/extensions/api.php
+++ src/extensions/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use JohannSchopplich\KirbyPlugins\FieldResolver;
+use JohannSchopplich\KirbyPlugins\ModelResolver;
 use Kirby\Cms\App;
-use Kirby\Cms\Find;
-use Kirby\Form\Form;
 
 return [
     'routes' => fn (App $kirby) => [
@@ -11,41 +11,8 @@ return [
             'method' => 'GET',
             'action' => function () use ($kirby) {
                 $id = $kirby->request()->query()->get('id');
-
-                // Decode encoded Panel view path
-                $model = match (true) {
-                    // See `filePattern` in Kirby's `config/api/routes/files.php`
-                    preg_match('!(account|pages\/[^\/]+|site|users\/[^\/]+)\/files\/(.+)!', $id, $matches) => Find::file(
-                        match (true) {
-                            str_starts_with($matches[1], 'pages/') => substr($matches[1], 6),
-                            str_starts_with($matches[1], 'users/') => substr($matches[1], 6),
-                            default => $matches[1]
-                        },
-                        $matches[2]
-                    ),
-                    str_starts_with($id, 'pages/') => Find::page(substr($id, 6)),
-                    $id === 'site' => $kirby->site(),
-                    default => null
-                };
-
-                $fields = $model->blueprint()->fields();
-                $languageCode = $model->kirby()->languageCode();
-                $content = $model->content($languageCode)->toArray();
-                $form = new Form([
-                    'fields' => $fields,
-                    'values' => $content,
-                    'model' => $model,
-                    'strict' => true
-                ]);
-
-                $fields = $form->fields()->toArray();
-                unset($fields['title'], $fields['slug']);
-
-                foreach ($fields as $index => $props) {
-                    unset($fields[$index]['value']);
-                }
-
-                return $fields;
+                $model = ModelResolver::resolveFromPath($id);
+                return FieldResolver::resolveModelFields($model);
             }
         ]
     ]
